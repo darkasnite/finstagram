8be71bee5212cb8730cff0b39e5d0268229a2e94
darkasnite <darkasnite@hotmail.com>
2021-02-21 20:23:51 +0000

    Week 6 Day 1

--- app/actions.rb
+++ app/actions.rb
@@ -4,10 +4,30 @@ helpers do
     end
 end
 
-get '/' do  
-    @finstagram_posts = FinstagramPost.order(created_at: :desc)
-    erb(:index)
+
+get '/finstagram_posts/new' do
+  @finstagram_post = FinstagramPost.new
+  erb(:"finstagram_posts/new")
 end
+
+post '/finstagram_posts' do
+  photo_url = params[:photo_url]
+
+  @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id })
+
+  if @finstagram_post.save
+    redirect(to('/'))
+  else
+    erb(:"finstagram_posts/new")
+  end
+
+end
+
+get '/finstagram_posts/:id' do
+  @finstagram_post = FinstagramPost.find(params[:id])
+  erb(:"finstagram_posts/show")
+end
+
 
 get '/signup' do
     @user = User.new
@@ -62,5 +82,37 @@ get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   @current_user = User.find_by(id: session[:user_id])
   erb(:index)
+
+end
+
+
+post '/comments' do
+
+  text = params[:text]
+  finstagram_post_id = params[:finstagram_post_id]
+
+  comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+
+  comment.save
+
+  redirect(back)
+
+end
+
+
+post '/likes' do
+
+  finstagram_post_id = params[:finstagram_post_id]
+  like = Like.new({ finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+  like.save
+  redirect(back)
+
+end
+
+delete '/likes/:id' do
+
+  like = Like.find(params[:id])
+  like.destroy
+  redirect(back)
 
 end
